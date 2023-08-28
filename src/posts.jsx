b5ae5516861fe4fa9cc931cd032198bd4085601b
smschijf <210578@student.glu.nl>
2023-08-28 11:12:07 +0200

    added search and filters

--- src/posts.jsx
+++ src/posts.jsx
@@ -17,8 +17,13 @@ const PostTitle = () => {
     return <span>{record ? `${record.title}` : ""}</span>
 }
 
+const postFilters = [
+    <TextInput source="q" label="Search" alwaysOn />,
+    <ReferenceInput source="userId" label="User" reference="users" />,
+];
+
 export const PostList = () => (
-  <List>
+  <List filters={postFilters}>
     <Datagrid rowClick="edit">
       <ReferenceField source="userId" reference="users" link="show" />
       <TextField source="id" />
