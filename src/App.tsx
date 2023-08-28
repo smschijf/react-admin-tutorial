5143a7b8e83c56b97d41e6913ff0291240f55fe0
smschijf <210578@student.glu.nl>
2023-08-28 11:14:40 +0200

    added menu icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import {
-  Admin,
-  Resource,
-  ShowGuesser
-} from "react-admin";
+import { Admin, Resource, ShowGuesser } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import { UserList } from "./users";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
 
-export const App = () => <Admin dataProvider={dataProvider}>
-  <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-  <Resource name="users" list={UserList} show={ShowGuesser} recordRepresentation="name" />
-</Admin>;
+export const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+      icon={PostIcon}
+    />
+    <Resource
+      name="users"
+      list={UserList}
+      show={ShowGuesser}
+      recordRepresentation="name"
+      icon={UserIcon}
+    />
+  </Admin>
+);
